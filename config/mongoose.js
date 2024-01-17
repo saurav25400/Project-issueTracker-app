a523043bec6c2d4467a8449d423a6c7a5dc69961
saurav25400 <86851522+saurav25400@users.noreply.github.com>
2024-01-17 11:56:28 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
-// const URI = process.env.DB_URI;
+const URI = process.env.DB_URI;
 
 launch().catch(err => console.log(`Error talking to MongoDB : ${err}`));
 
 async function launch(){
     // await mongoose.connect("mongodb://127.0.0.1:27017/issue_tracker");
-    await mongoose.connect("mongodb://localhost:27017/bugTrackerDb");
+    await mongoose.connect(URI);
 }
 
 const db = mongoose.connection.once('open', ()=>{
